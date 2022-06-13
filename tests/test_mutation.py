d4ca16e00b83ae4ae19791701dee073b6be2b197
Nima Parsa <sadev.inform@gmail.com>
2022-06-13 22:01:51 +0430

    ✅ Merge flip and swap mutation tests into one file

---
+++ tests/test_mutation.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from genetic.core import flip_mutate, swap_mutate
+
+
+class TestSwapMutation(TestCase):
+    def test_always_mutate(self):
+        chromosome = [1, 2, 3, 4, 5, 6]
+        chromosome_mutated = swap_mutate(chromosome, mutation_rate=1)
+        self.assertFalse(chromosome == chromosome_mutated)
+
+    def test_never_mutate(self):
+        chromosome = [1, 2, 3, 4, 5, 6]
+        chromosome_mutated = swap_mutate(chromosome, mutation_rate=0)
+        self.assertTrue(chromosome == chromosome_mutated)
+
+
+class TestFlipMutation(TestCase):
+    def test_always_mutate(self):
+        chromosome = [0, 0, 1, 0, 1, 1, 1, 0, 1]
+        chromosome_mutated = flip_mutate(chromosome, mutation_rate=1)
+        self.assertFalse(chromosome == chromosome_mutated)
+
+    def test_never_mutate(self):
+        chromosome = [0, 0, 1, 0, 1, 1, 1, 0, 1]
+        chromosome_mutated = flip_mutate(chromosome, mutation_rate=0)
+        self.assertTrue(chromosome == chromosome_mutated)
