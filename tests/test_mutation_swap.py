4bae4c7121cab29557f0233013f7e8e54620f21d
Nima Parsa <sadev.inform@gmail.com>
2022-06-13 19:10:05 +0430

    🖌️ Remove unnessary print functions

--- tests/test_mutation_swap.py
+++ tests/test_mutation_swap.py
@@ -7,8 +7,6 @@ class TestMutation(TestCase):
     def test_always_mutate(self):
         chromosome = [1, 2, 3, 4, 5, 6]
         chromosome_mutated = swap_mutate(chromosome, mutation_rate=1)
-        print(chromosome)
-        print(chromosome_mutated)
         self.assertFalse(chromosome == chromosome_mutated)
 
     def test_never_mutate(self):
