23aa33630559821ec9c6f4e7677bda486fb22c09
Nima Parsa <sadev.inform@gmail.com>
2022-06-15 17:00:17 +0430

    ✨ Add plotting functionality to utils

--- genetic/utils.py
+++ genetic/utils.py
@@ -1,7 +1,23 @@
-from typing import List
+from typing import Callable, List
+
+import matplotlib.pyplot as plt
 
 
 def cumulative_sum(array: List[float]) -> List[float]:
     for i in range(1, len(array)):
         array[i] += array[i - 1]
     return array
+
+
+def plot(
+    progress,
+    change: Callable = None,
+    xlabel: str = "Generation",
+    ylabel: str = "Fitness",
+):
+    if change:
+        progress = [change(item) for item in progress]
+    plt.plot(progress)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.show()
